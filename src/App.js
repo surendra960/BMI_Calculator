241da45c0b22886cee9a1a1235b12963bdc2a82b
unknown <smeena1@es.iitr.ac.in>
2023-07-12 05:29:44 +0530

    BMI calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import "./bmi.css";
 function App() {
+  // const [height, setHeight] = useState(0);
+  // const [weight, setWeight] = useState(0);
+
+  const [formData, setFormData ] = useState({height:0, weight : 0});
+  const [bmi, setBmi] = useState(0);
+
+  const calculateBMI = () => {
+    const heighiInMeter = formData.height / 100;
+    const customBmi = formData.weight / heighiInMeter ** 2;
+    console.log(customBmi);
+
+    setBmi(customBmi);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    console.log(name);
+
+  };
+
+
+
+  const resetFields = () => {
+    // setWeight(0);
+    // setHeight(0);
+    formData(0);
+    setBmi(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="inner">
+          <h1>BMI Calculator</h1>
+          <p className="discription">Weight in (Kg)</p>
+          <input
+            className="input1"
+            type="text"
+            value={formData.weight}
+            onChange={handleChange}
+          />
+          <p className="discription">Height (cm)</p>{" "}
+          <input
+            className="input2"
+            type="text"
+            value={formData.height}
+            onChange={handleChange}
+          />
+          <button className="common" onClick={calculateBMI} id="btn">
+            Submit
+          </button>
+          <button className="common" onClick={resetFields} id="Reload">
+            Reload
+          </button>
+          <h3>Your BMI is : {bmi.toFixed(1)}</h3>
+          <p className="discription">calculated {}</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/bmi.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './bmi.css'; // Import the CSS file
+
+function BMI() {
+  return (
+    <div className="container">
+        <h1>BMI Calculator</h1>
+    </div>
+  );
+}
+
+export default BMI;
